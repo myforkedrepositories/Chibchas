397b67dfb2af2f92ea1e15054a76dd98226e284d
RWC <david.medina1@udea.edu.co>
2021-04-28 19:46:26 -0500

    v0.0.1 work fine

--- chibchas/institulac.py
+++ chibchas/institulac.py
@@ -2,4 +2,4 @@ from tools import *
 if __name__=='__main__':
     user=input('usuario: ')
     password=getpass.getpass('Contraseña: ')
-    main(user,password,headless=False,start=0,end=1)
+    main(user,password,headless=False,start=1,end=2)
