ec83ce0f7abd634ee26555c9b4dec8485286e9bf
Diego Restrepo <restrepo@udea.edu.co>
2021-04-29 22:55:36 -0500

    lc

--- chibchas/institulac.py
+++
@@ -1,6 +0,0 @@
-from tools import *
-if __name__=='__main__':
-    s=time.time()
-    user=input('usuario: ')
-    password=getpass.getpass('Contraseña: ')
-    main(user,password,headless=False,start=1,end=2)
